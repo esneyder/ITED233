4c7748fcab2f04fbae8a418d7a8a45035178074e
Kevin Candelaria <kevdashdev@gmail.com>
2012-12-08 11:58:42 +0800

    Add comments to MP3.1.sql for documentation purposes.

--- Final/MP3/MP3.1.sql
+++ Final/MP3/MP3.1.sql
@@ -1,7 +1,9 @@
 DECLARE
+  /* Store the input values coming from the user */
   v_hire_date_from employees.hire_date%TYPE := &Hire_Date_From;
   v_hire_date_to employees.hire_date%TYPE := &Hire_Date_To;
   
+  /* Declare the variables which will hold the value from the cursor */
   v_hire_date employees.hire_date%TYPE;
   v_employee_id employees.employee_id%TYPE;
   v_last_name employees.last_name%TYPE;
@@ -9,19 +11,23 @@ DECLARE
   v_department_id employees.department_id%TYPE;
   v_department_name departments.department_name%TYPE;
   
+  /* Declare the variable which will hold the number of records returned by the cursor */
   v_record_counter NUMBER;
   
+  /* Cursor declaration, same as the one from MP2 */
   CURSOR v_emp_dept_cursor(p_hire_date_from v_hire_date_from%TYPE, p_hire_date_to v_hire_date_to%TYPE) IS
     SELECT e.hire_date, e.employee_id, e.last_name, e.first_name, e.department_id, d.department_name
     FROM employees e, departments d
     WHERE e.hire_date >= p_hire_date_from AND e.hire_date <= p_hire_date_to AND e.department_id = d.department_id;
   
+  /* Exception declarations */
   hire_date_error EXCEPTION;
   no_record_retrieved EXCEPTION;
   other_error EXCEPTION;
   
 BEGIN
 
+  /* Assign into v_record_counter the number of rows returned by the cursor, to track and handle the exception of not having any rows returned */
   SELECT COUNT(*) INTO v_record_counter FROM employees WHERE hire_date >= v_hire_date_from AND hire_date <= v_hire_date_to;
 
   IF v_hire_date_to < v_hire_date_from THEN
@@ -32,21 +38,21 @@ BEGIN
     OPEN v_emp_dept_cursor(v_hire_date_from, v_hire_date_to);
     DBMS_OUTPUT.PUT_LINE('RECORD/S THAT HAVE MET THE CRITERIA :');
     DBMS_OUTPUT.PUT_LINE('---------------------------------------');
-  LOOP
-    FETCH v_emp_dept_cursor INTO v_hire_date, v_employee_id, v_last_name, v_first_name, v_department_id, v_department_name;
-    IF v_emp_dept_cursor%NOTFOUND THEN
-      DBMS_OUTPUT.PUT_LINE('A total of ' || v_emp_dept_cursor%ROWCOUNT || ' record/s were listed.');
-      EXIT;
-    ELSE
-      DBMS_OUTPUT.PUT_LINE('Employee ID : ' || v_employee_id);
-      DBMS_OUTPUT.PUT_LINE('Last Name : ' || v_last_name);
-      DBMS_OUTPUT.PUT_LINE('First Name : ' || v_first_name);
-      DBMS_OUTPUT.PUT_LINE('Hire Date : ' || v_hire_date);
-      DBMS_OUTPUT.PUT_LINE('Department ID : ' || v_department_id);
-      DBMS_OUTPUT.PUT_LINE('Department Name : ' || v_department_name);
-      DBMS_OUTPUT.PUT_LINE('---------------------------------------');
-    END IF;
-  END LOOP;
+    LOOP
+      FETCH v_emp_dept_cursor INTO v_hire_date, v_employee_id, v_last_name, v_first_name, v_department_id, v_department_name;
+      IF v_emp_dept_cursor%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('A total of ' || v_emp_dept_cursor%ROWCOUNT || ' record/s were listed.');
+        EXIT;
+      ELSE
+        DBMS_OUTPUT.PUT_LINE('Employee ID : ' || v_employee_id);
+        DBMS_OUTPUT.PUT_LINE('Last Name : ' || v_last_name);
+        DBMS_OUTPUT.PUT_LINE('First Name : ' || v_first_name);
+        DBMS_OUTPUT.PUT_LINE('Hire Date : ' || v_hire_date);
+        DBMS_OUTPUT.PUT_LINE('Department ID : ' || v_department_id);
+        DBMS_OUTPUT.PUT_LINE('Department Name : ' || v_department_name);
+        DBMS_OUTPUT.PUT_LINE('---------------------------------------');
+      END IF;
+    END LOOP;
   ELSE
     RAISE other_error;
   END IF;
